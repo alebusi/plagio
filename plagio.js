f0a9b41327d1ec9b4c901f67bf13e1b61a48e6b4
alebusi <allebusi@gmail.com>
2021-01-07 17:57:47 +0100

    Update plagio.js

--- plagio.js
+++ plagio.js
@@ -6,13 +6,13 @@ function generaTesto() {
   ind=0;
   generaSequenza(); 
   testo="Titolo del libro "+document.getElementById("titolo").value+"...<br>";
-  testo=testo+"Autore "+document.getElementById("autore").value+"...<br>";
+  testo=testo+"Autore "+document.getElementById("autore").value+"...<br><br>";
   testo=testo+"Ecco le proposte...<br>";
   txtIncipit=document.getElementById("incipit").value;
   for (x=0; x < sequenza.length; x++) {
       testo=testo+(x+1)+": "+txtIncipit+" "+document.getElementById(sequenza[x]).value+"...<br>";
   }
-  testo=testo+"Ripetiamo per i meno attenti...<br>";
+  testo=testo+"<br>Ripetiamo per i meno attenti...<br><br>";
   testo=testo+document.getElementById("titolo").value+"...<br>";
   testo=testo+"di "+document.getElementById("autore").value+"...<br>";
   testo=testo+document.getElementById("incipit").value+"...<br>";
